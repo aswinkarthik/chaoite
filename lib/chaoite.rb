9b80c958421fc44a1b51cb4da6b702b3ef358adf
Aswin <aswinkarthik93@gmail.com>
2015-10-16 00:52:00 +0530

    #3[Aswin] Basic metric collection and posting to graphite server

--- lib/chaoite.rb
+++ lib/chaoite.rb
@@ -1,15 +1,66 @@
 require 'chaoite/version'
 require 'json'
 require 'chaoite/handler'
+require 'optparse'
+require 'graphite-api'
+require 'graphite-api/core_ext/numeric'
 
 module Chaoite
-  def self.run(args)
-    puts "Chaoite has started..."
-    configs = JSON.parse(File.read(args))
 
-    configs.each do |config|
-      Handler.send config["type"], config
+  DEFAULT_OPTIONS = {
+      :config_file => 'config.json',
+      :server => 'localhost',
+      :port => 2003
+  }
+
+  def self.run(args)
+
+    options = DEFAULT_OPTIONS
+
+    parser = setup_parser(options)
+    parser.parse!
+
+    configs = JSON.parse(File.read(options[:config_file]))
+
+    client = GraphiteAPI.new(:graphite => "#{options[:server]}:#{options[:port]}")
+
+    client.every 10.seconds do |c|
+      configs.each do |config|
+        metric = Handler.send("#{config["type"]}_#{config["value"]}", config)
+        puts "Sending metric #{metric}"
+        c.metrics(metric)
+      end
     end
 
+    begin
+      sleep 2 while true
+    rescue Interrupt => e
+      puts "Shutting down Chaoite"
+    end
+
+  end
+
+  def self.setup_parser(options)
+    OptionParser.new do |opts|
+      opts.banner = "Usage: chaoite [options]"
+
+      opts.on('-c', '--config-file config_file', "Path to config file. Defaults to #{options[:config_file]}") do |config_file|
+        options[:config_file] = config_file
+      end
+
+      opts.on('-g', '--graphite-server server', "Graphite server url. Defaults to #{options[:server]}") do |server|
+        options[:server] = server
+      end
+
+      opts.on('-p', '--graphite-port port', "Graphite port. Defaults to #{options[:port]}") do |port|
+        options[:port] = port
+      end
+
+
+      opts.on('-h', '--help', 'Displays Help') do
+        puts opts
+        exit
+      end
+    end
   end
 end
--- lib/chaoite/handler.rb
+++ lib/chaoite/handler.rb
@@ -1,13 +1,28 @@
 require 'net/http'
 
 module Handler
-  def self.http config
-    puts "Http handler invoked"
-    res = Net::HTTP.get_response(URI(config["url"]))
+  def self.http_state config
 
-    if res.code == '200'
-      puts "App is up"
+    key = config["key"]
+    begin
+      res = Net::HTTP.get_response(URI(config["url"]))
+      value = res.code == '200' ? 1 : 0
+    rescue
+      value = 0
     end
-
+    return  key => value
   end
+
+  def self.http_metric config
+
+    key = config["key"]
+    begin
+      res = Net::HTTP.get_response(URI(config["url"]))
+      value = res.body
+    rescue
+      value = nil
+    end
+    return  key => value
+  end
+
 end
